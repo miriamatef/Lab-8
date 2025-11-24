dc6e0111b67e3946ebd2cf809e7a90d5014bd8c6
mariamelshamy-20 <marybasemad@gmail.com>
2025-11-24 02:23:22 +0200

    Add files via upload

--- Question.java
+++ Question.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lab7.isa;
+package skillforge;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
